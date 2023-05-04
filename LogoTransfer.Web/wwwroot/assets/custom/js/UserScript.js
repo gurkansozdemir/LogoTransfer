4f3847df1ab3307b2fea49408a0a234081ace6c8
Gurkan Sozdemir <gurkansozdemir@hotmail.com.tr>
2023-05-04 12:35:49 +0300

    AllUser

--- LogoTransfer.Web/wwwroot/assets/custom/js/UserScript.js
+++ LogoTransfer.Web/wwwroot/assets/custom/js/UserScript.js
@@ -18,14 +18,7 @@
             { data: 'lastName' },
             { data: 'userName' },
             { data: 'eMail' },
-            { data: 'password' },
-            {
-                data: 'Process',
-                "render": function (data, type, full, meta) {
-                    return `<a class="btn btn-sm btn-custom-warning" data-toggle="modal" data-target="#editStudentModal" onclick="editStudentModal(` + full['id'] + `);"><i class="la la-pencil"></i></a>
-                            <a href="javascript:void(0);" class="btn btn-sm btn-custom-danger sweet-success-cancel" onclick="DeleteUser(`+ full['id'] + `,'studentTable');"><i class="la la-trash-o"></i></a>`;
-                }
-            }
+            { data: 'password' }
         ],
         "language": {
             "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
