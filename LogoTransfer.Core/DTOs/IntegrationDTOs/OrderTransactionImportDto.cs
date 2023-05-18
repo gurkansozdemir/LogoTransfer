b09aaf8cad45efa1954cf185566fe1a3c3606954
Gurkan Sozdemir <gurkansozdemir@hotmail.com.tr>
2023-05-18 20:48:23 +0300

    Order Import Complated..

--- LogoTransfer.Core/DTOs/IntegrationDTOs/OrderTransactionImportDto.cs
+++ LogoTransfer.Core/DTOs/IntegrationDTOs/OrderTransactionImportDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LogoTransfer.Core.DTOs.IntegrationDTOs
+﻿namespace LogoTransfer.Core.DTOs.IntegrationDTOs
 {
     public class OrderTransactionImportDto
     {
